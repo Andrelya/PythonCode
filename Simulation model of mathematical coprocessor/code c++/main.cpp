5ee74bac9696f2bcd672b022be2ebfbb678f38d9
Andrelya <91786420+Andrelya@users.noreply.github.com>
2022-03-15 00:43:45 +0200

    Create main.cpp

---
+++ Simulation model of mathematical coprocessor/code c++/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "binary.hpp"
+
+using namespace std;
+int main(int argc, const char * argv[]) {
+    
+    //input();
+    //double a = with_binary_in_chyslo_not_int();
+    //cout << a << endl;
+    input();
+    
+    
+    return 0;
+}
