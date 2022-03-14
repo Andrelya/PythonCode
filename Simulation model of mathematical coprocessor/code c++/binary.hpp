6ea0d506819a9a75b32e0bb7bf49dca4d035174d
Andrelya <91786420+Andrelya@users.noreply.github.com>
2022-03-15 00:09:31 +0200

    Create binary.hpp

---
+++ Simulation model of mathematical coprocessor/code c++/binary.hpp
@@ -0,0 +1,15 @@
+#ifndef binary_hpp
+#define binary_hpp
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+#include <math.h>
+#include <regex>
+
+using namespace std;
+
+void input();
+
+#endif /* binary_hpp */
